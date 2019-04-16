7288e563a88239f03462314b8ba8ec7dd6f03d19
adamelsbury <adam.john.elsbury@gmail.com>
2019-04-16 22:37:09 +0100

    test PHP script working

--- pages/test1service.php
+++ pages/test1service.php
@@ -1,18 +1,25 @@
-<?
+<?php
 
-$conn = new PDO ("mysql:host=localhost;dbname=ephp086;", "ephp086", "ipeithux");
 
-$id = $_POST["ID"];
 
-$results = $conn->query("UPDATE test1 SET nonfacialcue = nonfacialcue +1");
-$results2 = $conn->query("UPDATE test1 SET facialcue = facialcue +1");
+$conn = new PDO ("mysql:host=sql107.epizy.com;dbname=epiz_23773903_testenvironmentdb;", "epiz_23773903", "IYSNctQrvboxG7S");
 
-if("$id = 'nonfacialcue'") {
-  $results;
-  header("test2.html");
-}
+$conn->query("INSERT INTO test1 (nonfacialcue, facialcue) VALUES ('1', '0')");
+header("location: test2.html");
+?>
 
-if("$id = 'facialcue'") {
-  $results2;
-  header("test2.html");
-}
+//$conn = new PDO ("mysql:host=sql107.epizy.com;dbname=epiz_23773903_testenvironmentdb;", "epiz_23773903", "IYSNctQrvboxG7S");
+
+
+
+//$conn->query("UPDATE test1 SET NonFacialCue = NonFacialCue + 1");
+
+//$conn->query("INSERT INTO test1 (nonfacialcue, facialcue) VALUES ('1', '0')");
+//$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //  echo "Connected successfully";
+  //  }
+//catch(PDOException $e)
+    //{
+    //echo "Connection failed: " . $e->getMessage();
+    //}
+?>
