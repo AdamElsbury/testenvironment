1d0b802201b518dccb18e0c7ba3d0d03e291d096
adamelsbury <adam.john.elsbury@gmail.com>
2019-04-17 02:18:25 +0100

    Test environment complete

--- pages/test3left.php
+++ pages/test3left.php
@@ -4,6 +4,6 @@
 
 $conn = new PDO ("mysql:host=sql107.epizy.com;dbname=epiz_23773903_testenvironmentdb;", "epiz_23773903", "IYSNctQrvboxG7S");
 
-$conn->query("INSERT INTO test3 (left, right) VALUES ('1', '0')");
+$conn->query("INSERT INTO test3 (rightcue, leftcue) VALUES ('0', '1')");
 header("location: test4.html");
 ?>
--- pages/test5nfc.php
+++ pages/test5nfc.php
@@ -5,5 +5,5 @@
 $conn = new PDO ("mysql:host=sql107.epizy.com;dbname=epiz_23773903_testenvironmentdb;", "epiz_23773903", "IYSNctQrvboxG7S");
 
 $conn->query("INSERT INTO test5 (nonfacialcue, facialcue) VALUES ('1', '0')");
-header("location: sucess.html");
+header("location: success.html");
 ?>
